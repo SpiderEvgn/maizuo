d4be8f0d537f1b48bfa68f60c2f62ea766dcc845
SpiderEvgn <xiajun.zhang@usingnow.com>
2016-03-13 18:07:51 +0800

    add auto timestamp

--- app/models/mzapi.rb
+++ app/models/mzapi.rb
@@ -5,20 +5,26 @@ class Mzapi < ActiveRecord::Base
   # default_params 
   format :json
 
+  
+
   def self.getcinemas
     # 拉取城市和影院列表
-    sign_value = Digest::MD5.hexdigest("client_id=52642103681&timestamp=20160313173012&key=xkGEr244(((<HAee4346fg")
+    time = Time.new
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    sign_value = Digest::MD5.hexdigest("client_id=52642103681&timestamp=#{timestamp}&key=xkGEr244(((<HAee4346fg")
     get("/rest/ticket3.0/cinemas", query: { client_id: "52642103681", 
-                                            timestamp: "20160313173012", 
+                                            timestamp: "#{timestamp}", 
                                             sign: "#{sign_value}"})
   end
 
   def self.getfilms
     # 拉取电影列表
-    sign_value = Digest::MD5.hexdigest("client_id=52642103681&timestamp=20160313173012&key=xkGEr244(((<HAee4346fg")
+    time = Time.new
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    sign_value = Digest::MD5.hexdigest("client_id=52642103681&timestamp=#{timestamp}&key=xkGEr244(((<HAee4346fg")
     get("/rest/ticket3.0/films", query: { client_id: "52642103681", 
                                           sign: "#{sign_value}", 
-                                          timestamp: "20160313173012", 
+                                          timestamp: timestamp, 
                                           start: "0", 
                                           count: "10", 
                                           isShow: "1", 
